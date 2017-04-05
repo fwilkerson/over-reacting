743a7d777708bfb02f67488d38d1e3c66547fbcc
Frank Wilkerson <FWilkerson@ad.exetered.com>
2017-04-04 18:56:19 -0700

    added stateful component example

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,14 +1,18 @@
 import React from 'react'
 
 import Header from './Header'
+import Tabs from './Tabs'
+import Tab from './Tab'
 
 const Root = () => (
 	<div className="container">
 		<div className="row">
 			<div className="eight columns offset-by-two">
 				<Header title="Components" />
-				<Header title="Stateless" />
-				<Header title="Stateful?" />
+				<Tabs selectedTabChanged={(index) => console.log(index)}>
+					<Tab>stateless</Tab>
+					<Tab text="stateful" active />
+				</Tabs>
 			</div>
 		</div>
 	</div>
